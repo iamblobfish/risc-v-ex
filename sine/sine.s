ad5c1f806dac45d2f2e5a90e9686230957dd811c
iamblobfish <51192701+iamblobfish@users.noreply.github.com>
2023-08-13 04:01:11 +0300

    fair sine.s

--- sine/sine.s
+++ sine/sine.s
@@ -10,10 +10,12 @@
 #	a2 -- output string buffer for the string result
 sine:
 	lb t0, 0(a1)
-	li 2(a2), '0'
-  	ble '0', t0, output
-   	sb 2(a1), 2(a2)
-output:
- 	sb t0, 0(a2)
-  	li 1(a2), '.'
-    	ret
+	li t2, '0'
+  	bgt t0, t2, output
+    	lb t2, 2(a1)
+ output:
+ 	li t1, '.'
+  	sb t0, 0(a2)
+   	sb t1, 1(a2)
+    	sb t2, 2(a2)
+     	ret
