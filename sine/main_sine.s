cd4ac732cac8c8ecafd8afeaa46cb8db2346f7c8
iamblobfish <51192701+iamblobfish@users.noreply.github.com>
2023-08-13 13:42:05 +0300

    print main_sine.s

--- sine/main_sine.s
+++ sine/main_sine.s
@@ -1,28 +1,51 @@
 # syscalls
-exit = 93
+exit    = 93
+read 	= 63
+write	= 64
+ds	= 0x20000
 
-.section .data
+.section .data 
+
+# This is  0-ended string with input data
+input:
+.align 4
+.space 100
 
 # This will be used for 0-ended string with result. Use "-1" if you cannot calculate the function
-output:
-    .align 4
-    .space 100
+output:                
+.align 4
+.space	100
+  
 
-.section .text
+.section .text 
 .globl _start
 
-_start:
+_start:     
 
-    # Buffer initialization will be here
+	# gp initialization
+	li	gp, ds
 
-    # Load the address of the input string from the command line argument
-    mv a1, a0
+	# Read
+	li	a7, 63
+	li	a0, 0
+	la	a1, input
+	li	a2, 100
+	ecall
+	
+	# Buffer initialisation will be here
 
-    la a2, output
-    call sine
+	la	a1, input
+	la	a2, output
+	call 	sine
 
-    # Result checking will be here
+	# Result checking will be here
+	
+	li	a7, 64
+	li	a0, 1
+	la	a1, output
+	li	a2, 100
+	ecall
 
-    li a0, 0
-    li a7, exit
-    ecall
+	li	a0, 0
+	li	a7, exit
+	ecall
