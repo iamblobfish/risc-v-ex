d560a480765d8dee4ab5087d2cfd7b1798e59b43
iamblobfish <51192701+iamblobfish@users.noreply.github.com>
2023-08-13 05:19:52 +0300

    bye sine.s

--- sine/sine.s
+++ sine/sine.s
@@ -1,21 +1,15 @@
 .globl sine
 
 .section .data
-default_value:
-    .asciz "0.0"
-    
-one_value:
-    .asciz "1.0"
-    
 
 .section .text
 .globl main
 
 sine:
-    la  a2, default_value  
-    lb t0, 0(a1)
- 	  li t2, '0'
-   	bgt t0, t2, output
-    la  a2, one_value 
+    sw a1, 0(a2)
+  	li t2, '0'
+    bge t0, t2, output
+    lb t2, 2(a1)
 output:
+    sb t2, 2(a2)
     ret
