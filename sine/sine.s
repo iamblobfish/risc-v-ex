5dbe8447d9cc97057f2fa5ec2a9b2cbde8668097
iamblobfish <51192701+iamblobfish@users.noreply.github.com>
2023-08-13 12:07:49 +0300

    fare sine.s

--- sine/sine.s
+++ sine/sine.s
@@ -6,10 +6,13 @@
 .globl main
 
 sine:
-    sw a1, 0(a2)
-    lb t0, 0(a1)
-  	li t2, '0'
-    ble t0, t2, output
+ 	lb t0, 0(a1)
+ 	li t2, '0'
+   	bgt t0, t2, output
+     	lb t2, 2(a1)
+  output:
+  	li t1, '.'
+   	sb t0, 0(a2)
+    sb t1, 1(a2)
     sb t2, 2(a2)
-output:
     ret
