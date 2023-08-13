d155178c529e69f3a101201d431f4088bb378612
iamblobfish <51192701+iamblobfish@users.noreply.github.com>
2023-08-13 04:39:54 +0300

    Update sine.s

--- sine/sine.s
+++ sine/sine.s
@@ -9,12 +9,5 @@
 #	a1 -- input buffer will contain string with the argument
 #	a2 -- output string buffer for the string result
 sine:
-	li t2, '0'
-  	bgt 0(a1), t2, output
-    	lb t2, 2(a1)
- output:
- 	li t1, '.'
-  	sb 0(a1), 0(a2)
-   	sb t1, 1(a2)
-    	sb t2, 2(a2)
+  	sb a1, a2
      	ret
